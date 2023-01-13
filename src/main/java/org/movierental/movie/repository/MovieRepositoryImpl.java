b2ef4f77cd3a66b9942f42e98da19b64ef6d3697
F1iper <ftimofiejew@gmail.com>
2023-01-13 11:35:57 +0100

    Add movie package docs

--- src/main/java/org/movierental/movie/repository/MovieRepositoryImpl.java
+++ src/main/java/org/movierental/movie/repository/MovieRepositoryImpl.java
@@ -11,6 +11,12 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * The MovieRepositoryImpl is a concrete implementation of the MovieRepository interface.
+ * It provides methods for performing CRUD operations on Movie objects in the database.
+ *
+ * @author Filip Timofiejew
+ */
 public class MovieRepositoryImpl implements MovieRepository {
 
     private final static String MOVIES = "movies";
@@ -18,6 +24,12 @@ public class MovieRepositoryImpl implements MovieRepository {
     private final static String STATUS = "status";
     private final static String LANGUAGE = "language";
 
+    /**
+     * Add a movie to the database
+     *
+     * @param movie the movie to add
+     * @return true if the movie was successfully added, false otherwise
+     */
     @Override
     public boolean add(Movie movie) {
         try (var queryExecution = new QueryExecutor();
@@ -46,61 +58,132 @@ public class MovieRepositoryImpl implements MovieRepository {
         }
     }
 
+    /**
+     * Find movies by title
+     *
+     * @param title the title of the movie to search for
+     * @return a list of movies that match the given title
+     */
     @Override
     public List<Movie> findByTitle(String title) {
         return execute("SELECT * FROM " + MOVIES + " WHERE title LIKE '%" + title + "%'");
     }
 
+    /**
+     * Find a movie by ID
+     *
+     * @param id the ID of the movie to search for
+     * @return the movie that matches the given ID, or null if no match is found
+     */
     @Override
     public Movie findById(Long id) {
         return executeFindById("SELECT * FROM " + MOVIES + " WHERE movie_id = " + id);
     }
 
+    /**
+     * Find movies by movie type ID
+     *
+     * @param movieTypeId the ID of the movie type to search for
+     * @return a list of movies that match the given movie type ID
+     */
     @Override
     public List<Movie> findByMovieTypeId(Long movieTypeId) {
         return execute("SELECT * FROM " + MOVIES + " WHERE movie_type_id = " + movieTypeId);
     }
 
+    /**
+     * Find movies by release year
+     *
+     * @param year the release year of the movies to search for
+     * @return a list of movies that were released in the given year
+     */
     @Override
     public List<Movie> findByReleaseYear(int year) {
         return execute("SELECT * FROM " + MOVIES + " WHERE release_year = " + year);
     }
 
+    /**
+     * Update the title of a movie
+     *
+     * @param id    the ID of the movie to update
+     * @param title the new title for the movie
+     * @return true if the update was successful, false otherwise
+     */
     @Override
     public boolean updateTitle(Long id, String title) {
         return executeUpdate("UPDATE " + MOVIES + " SET title = '" + title + "' WHERE movie_id = " + id);
     }
 
+    /**
+     * Update the description of a movie
+     *
+     * @param id          the ID of the movie to update
+     * @param description the new description for the movie
+     * @return true if the update was successful, false otherwise
+     */
     @Override
     public boolean updateDescription(Long id, String description) {
         return executeUpdate("UPDATE " + MOVIES + " SET description = '" + description + "' WHERE movie_id = " + id);
     }
 
+    /**
+     * Remove a movie by ID
+     *
+     * @param id the ID of the movie to remove
+     * @return true if the removal was successful, false otherwise
+     */
     @Override
     public boolean removeById(Long id) {
         return executeRemove("DELETE FROM " + MOVIES + " WHERE movie_id = " + id);
     }
 
+    /**
+     * Find all the statuses of the movie
+     *
+     * @return List of statuses available
+     */
     @Override
     public List<Status> findStatuses() {
         return statuses("SELECT * FROM " + STATUS);
     }
 
+    /**
+     * Find all the movie types available
+     *
+     * @return List of movie types
+     */
     @Override
     public List<MovieType> findMovieTypes() {
         return movieTypes("SELECT * FROM " + MOVIE_TYPE);
     }
 
+    /**
+     * Find all the languages available
+     *
+     * @return List of languages
+     */
     @Override
     public List<Language> findLanguages() {
         return languages("SELECT * FROM " + LANGUAGE);
     }
 
+    /**
+     * Find all the movies by cost range
+     *
+     * @param min the minimum cost of the movie
+     * @param max the maximum cost of the movie
+     * @return List of movies with in the cost range
+     */
     @Override
     public List<Movie> findByCostRange(double min, double max) {
         return execute("SELECT * FROM " + MOVIES + " WHERE cost BETWEEN " + min + " AND " + max);
     }
 
+    /**
+     * Find all the movies available in the system
+     *
+     * @return List of all movies
+     */
     @Override
     public List<Movie> findAll() {
         return execute("SELECT * FROM " + MOVIES);
@@ -120,6 +203,12 @@ public class MovieRepositoryImpl implements MovieRepository {
         return movies;
     }
 
+    /**
+     * Execute a query for updating the movie details
+     *
+     * @param sql sql query to be executed
+     * @return true if the update was successful, false otherwise
+     */
     private boolean executeUpdate(String sql) {
         try (var queryExecution = new QueryExecutor();
              var connection = queryExecution.getConnection();
@@ -132,10 +221,16 @@ public class MovieRepositoryImpl implements MovieRepository {
         return false;
     }
 
+    /**
+     * Execute a query for removing the movie from the system
+     *
+     * @param sql sql query to be executed
+     * @return true if the removal was successful, false otherwise
+     */
     private boolean executeRemove(String sql) {
         try (var queryExecution = new QueryExecutor();
-        var connection = queryExecution.getConnection();
-        var statement = connection.createStatement()) {
+             var connection = queryExecution.getConnection();
+             var statement = connection.createStatement()) {
             int rowsAffected = statement.executeUpdate(sql);
             return rowsAffected > 0;
         } catch (SQLException e) {
@@ -144,6 +239,12 @@ public class MovieRepositoryImpl implements MovieRepository {
         return false;
     }
 
+    /**
+     * Execute a query for finding the movie by id
+     *
+     * @param sql sql query to be executed
+     * @return movie object corresponding to the given id
+     */
     private Movie executeFindById(String sql) {
         Movie movie = new Movie();
         try (var queryExecution = new QueryExecutor();
@@ -158,6 +259,12 @@ public class MovieRepositoryImpl implements MovieRepository {
         return movie;
     }
 
+    /**
+     * Create movie object from the query result
+     *
+     * @param rs query result
+     * @return movie object
+     */
     private Movie movieCreator(ResultSet rs) throws SQLException {
         Long id = rs.getLong("movie_id");
         String title = rs.getString("title");
@@ -173,7 +280,12 @@ public class MovieRepositoryImpl implements MovieRepository {
         return new Movie(id, title, description, releaseYear, length, languageId, cost, statusId, rentalRate, movieTypeId);
     }
 
-
+    /**
+     * Execute a query for finding the movie types
+     *
+     * @param sql sql query to be executed
+     * @return list of movie types
+     */
     private List<MovieType> movieTypes(String sql) {
         List<MovieType> movieTypes = new ArrayList<>();
         try (var queryExecution = new QueryExecutor();
@@ -191,6 +303,12 @@ public class MovieRepositoryImpl implements MovieRepository {
         return movieTypes;
     }
 
+    /**
+     * Execute a query for finding the statuses of the movies
+     *
+     * @param sql sql query to be executed
+     * @return list of statuses
+     */
     private List<Status> statuses(String sql) {
         List<Status> statuses = new ArrayList<>();
         try (var queryExecution = new QueryExecutor();
@@ -208,6 +326,12 @@ public class MovieRepositoryImpl implements MovieRepository {
         return statuses;
     }
 
+    /**
+     * Execute a query for finding the languages of the movies
+     *
+     * @param sql sql query to be executed
+     * @return list of languages
+     */
     private List<Language> languages(String sql) {
         List<Language> languages = new ArrayList<>();
         try (var queryExecution = new QueryExecutor();
--- src/main/java/org/movierental/movie/service/MovieServiceImpl.java
+++ src/main/java/org/movierental/movie/service/MovieServiceImpl.java
@@ -9,73 +9,155 @@ import org.movierental.movie.repository.MovieRepository;
 
 import java.util.List;
 
+/**
+ * Implementation of the {@link MovieService} interface
+ *
+ * @author Filip Timofiejew
+ */
 @RequiredArgsConstructor
 public class MovieServiceImpl implements MovieService {
 
     private final MovieRepository movieRepository;
 
+    /**
+     * This method is used to add a movie to the movie repository.
+     *
+     * @param movie The movie object that needs to be added.
+     * @return true if movie is added successfully, false otherwise
+     */
     @Override
     public boolean add(Movie movie) {
         return movieRepository.add(movie);
     }
 
+    /**
+     * This method is used to find movies by title
+     *
+     * @param name The title of the movie
+     * @return The list of movies with the given title
+     */
     @Override
     public List<Movie> findByTitle(String name) {
         return movieRepository.findByTitle(name);
     }
 
+    /**
+     * This method is used to find a movie by id
+     *
+     * @param id The id of the movie
+     * @return The movie with the given id
+     */
     @Override
     public Movie findById(Long id) {
         return movieRepository.findById(id);
     }
 
+    /**
+     * This method is used to find movies by movie type id
+     *
+     * @param categoryId The id of the movie type
+     * @return The list of movies with the given movie type id
+     */
     @Override
     public List<Movie> findByMovieTypeId(Long categoryId) {
         return movieRepository.findByMovieTypeId(categoryId);
     }
 
+    /**
+     * This method is used to find movies by release year
+     *
+     * @param year The release year of the movie
+     * @return The list of movies with the given release year
+     */
     @Override
     public List<Movie> findByReleaseYear(int year) {
         return movieRepository.findByReleaseYear(year);
     }
 
+    /**
+     * This method is used to update the title of a movie
+     *
+     * @param id   The id of the movie to update
+     * @param name The new title of the movie
+     * @return true if update is successful, false otherwise
+     */
     @Override
     public boolean updateTitle(Long id, String name) {
         return movieRepository.updateTitle(id, name);
     }
 
+    /**
+     * This method is used to update the description of a movie
+     *
+     * @param id          The id of the movie to update
+     * @param description The new description of the movie
+     * @return true if update is successful, false otherwise
+     */
     @Override
     public boolean updateDescription(Long id, String description) {
         return movieRepository.updateDescription(id, description);
     }
 
+    /**
+     * This method is used to remove a movie by id
+     *
+     * @param id The id of the movie to remove
+     * @return true if removal is successful, false otherwise
+     */
     @Override
     public boolean removeById(Long id) {
         return movieRepository.removeById(id);
     }
 
+    /**
+     * This method is used to find all statuses of movies
+     *
+     * @return The list of statuses
+     */
     @Override
     public List<Status> findStatuses() {
         return movieRepository.findStatuses();
     }
 
+    /**
+     * This method is used to find all movie types
+     *
+     * @return The list of movie types
+     */
     @Override
     public List<MovieType> findMovieTypes() {
         return movieRepository.findMovieTypes();
     }
 
+    /**
+     * This method is used to find all languages
+     *
+     * @return The list of languages
+     */
     @Override
     public List<Language> findLanguages() {
         return movieRepository.findLanguages();
     }
 
+    /**
+     * This method is used to find movies by cost range
+     *
+     * @param min The minimum cost of the movie
+     * @param max The maximum cost of the movie
+     * @return The list of movies with the given cost range
+     */
     @Override
     public List<Movie> findByCostRange(double min, double max) {
         return movieRepository.findByCostRange(min, max);
     }
 
+    /**
+     * This method is used to find all movies
+     *
+     * @return The list of all movies
+     */
     @Override
     public List<Movie> findAll() {
-        return  movieRepository.findAll();
+        return movieRepository.findAll();
     }
 }
